4e2c321a017819cdcd28cde1f4c9f98366107f94
Carlos Rocha <207734014+CarlosRocha2507@users.noreply.github.com>
2025-04-17 11:26:52 +0100

    Router and 404 page
    Correction of router and .htaccess.
    New 404 page.

---
+++ app/private/components/footer.php
@@ -0,0 +1,41 @@
+<style>
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+    }
+
+    .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        padding: 1rem 0;
+    }
+</style>
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>This site is maded for fun</p>
+        <hr class="navbar-divider">
+        <p>Copyright &copy; 2023 </p>
+        <a href="https://www.facebook.com" target="_blank" aria-label="Facebook">
+            <span class="icon">
+                <i class="fab fa-facebook" aria-hidden="true"></i>
+            </span>
+        </a>
+        <a href="https://www.twitter.com" target="_blank" aria-label="Twitter">
+            <span class="icon">
+                <i class="fab fa-twitter" aria-hidden="true"></i>
+            </span>
+        </a>
+        <a href="https://www.instagram.com" target="_blank" aria-label="Instagram">
+            <span class="icon">
+                <i class="fab fa-instagram" aria-hidden="true"></i>
+            </span>
+        </a>
+        <a href="https://www.linkedin.com" target="_blank" aria-label="LinkedIn">
+            <span class="icon">
+                <i class="fab fa-linkedin" aria-hidden="true"></i>
+            </span>
+        </a>
+    </div>
+</footer>
